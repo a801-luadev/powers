65b9f03c9e12eb6fc600c45b50a79b99e8d0d949
Lautenschlager-id <tai.romani@hotmail.com>
2020-05-09 21:37:41 -0300

    set up eventKeyboard powers

--- src/events/eventKeyboard.lua
+++ src/events/eventKeyboard.lua
@@ -2,5 +2,32 @@ eventKeyboard = function(playerName, key, isDown, x, y)
 	if not playerCanTriggerEvent(playerName) then return end
 
 	local cache = playerCache[playerName]
-	cache.keySequence:insert(key)
+
+	local playerKs = cache.keySequence
+	playerKs:insert(key)
+
+	local time = time()
+
+	local matchCombo = false
+	local src = Power.__keyboard
+	for power = 1, Power.__eventCount.__keyboard do
+		power = src[power]
+
+		-- Not internal, must be explicit
+		if power.triggererKey then
+			matchCombo = (key == power.triggererKey)
+		else
+			local powerKs = power.keySequences
+			for ks = 1, power.totalKeySequences do
+				if playerKs:isEqual(powerKs[ks]) then
+					matchCombo = true
+					break
+				end
+			end
+		end
+
+		if matchCombo then
+			return power:trigger(playerName, cache, time, x, y)
+		end
+	end
 end
