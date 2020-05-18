7b01adb00455aeea8102a1adb06d90c5afedbc38
Lautenschlager-id <tai.romani@hotmail.com>
2020-05-18 12:40:44 -0300

    add kills data

--- src/api/life.lua
+++ src/api/life.lua
@@ -5,23 +5,29 @@ local damage = function(playerName, damage, _cache)
 	if _cache.health <= 0 then
 		killPlayer(playerName)
 		removeLifeBar(playerName)
+		return true
 	else
 		updateLifeBar(playerName, _cache.health)
-	end
-end
-
-local damagePlayers = function(except, damage, filter, x, y, ...)
-	for name, cache in next, getPlayersOnFilter(except, filter, x, y, ...) do
-		damage(name, damage, cache)
+		return false
 	end
 end
 
 local damagePlayersWithAction = function(except, damage, action, filter, x, y, ...)
+	local hasKilled = false
 	for name, cache in next, getPlayersOnFilter(except, filter, x, y, ...) do
-		if action(name) then
-			damage(name, damage, cache)
+		if (not action and true or action(name))
+			and damage(name, damage, cache) then -- Has died
+			hasKilled = true
+			playerData:set(except, "kills", 1, true)
 		end
 	end
+	if hasKilled then
+		playerData:save(except)
+	end
+end
+
+local damagePlayers = function(except, damage, filter, x, y, ...)
+	return damagePlayersWithAction(except, damage, nil, filter, x, y, ...)
 end
 
 local addHealth = function(playerName, cache, hp)
