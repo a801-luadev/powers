38ac51862d5a8a4ee62e72887724f41934243380
Lautenschlager-id <tai.romani@hotmail.com>
2020-06-10 16:06:05 -0300

    fix write and read 8

--- src/classes/byteArray.lua
+++ src/classes/byteArray.lua
@@ -28,7 +28,7 @@ do
 		}, byteArray)
 	end
 
-	byteArray.write8 = function(self, ...)
+	byteArray.write = function(self, ...)
 		local bytes = { ... }
 
 		local bytesLen = #bytes
@@ -47,8 +47,15 @@ do
 		return self
 	end
 
+	byteArray.write8 = function(self, byte)
+		return self:write(
+			rshift(byte, 7) % 2, -- (byte >> 7) & 1
+			byte
+		)
+	end
+
 	byteArray.write16 = function(self, short)
-		return self:write8(
+		return self:write(
 			rshift(short, 14) % 4, -- (short >> 14) & 3
 			rshift(short, 7), -- short >> 7
 			short
@@ -56,7 +63,7 @@ do
 	end
 
 	byteArray.write24 = function(self, int)
-		return self:write8(
+		return self:write(
 			rshift(int, 21) % 8, -- (int >> 21) & 7
 			rshift(int, 14), -- int >> 14
 			rshift(int, 7), -- int >> 7
@@ -65,7 +72,7 @@ do
 	end
 
 	byteArray.write32 = function(self, long)
-		return self:write8(
+		return self:write(
 			rshift(long, 28) % 16, -- (long >> 28) & 15
 			rshift(long, 21), -- long >> 21
 			rshift(long, 14), -- long >> 14
@@ -104,7 +111,7 @@ do
 		return self:write8(bool and 1 or 0)
 	end
 
-	byteArray.read8 = function(self, quantity)
+	byteArray.read = function(self, quantity)
 		quantity = quantity or 1
 
 		local stackReadPos = self.stackReadPos + quantity
@@ -124,28 +131,34 @@ do
 		return (quantity == 1 and byteStack[1] or byteStack)
 	end
 
+	byteArray.read8 = function(self)
+		local byteStack = self:read(2)
+		-- b[1] << 7 + b[2]
+		return lshift(byteStack[1], 7) + byteStack[2]
+	end
+
 	byteArray.read16 = function(self)
-		local shortStack = self:read8(3)
+		local shortStack = self:read(3)
 		-- s[1] << 14 + s[2] << 7 + s[3]
 		return lshift(shortStack[1], 14) + lshift(shortStack[2], 7) + shortStack[3]
 	end
 
 	byteArray.read24 = function(self)
-		local intStack = self:read8(4)
+		local intStack = self:read(4)
 		-- i[1] << 21 + i[2] << 14 + i[3] << 7 + i[4]
 		return lshift(intStack[1], 21) + lshift(intStack[2], 14) + lshift(intStack[3], 7)
 			+ intStack[4]
 	end
 
 	byteArray.read32 = function(self)
-		local longStack = self:read8(5)
+		local longStack = self:read(5)
 		-- l[1] << 28 + l[2] << 21 + l[3] << 14 + l[4] << 7 + l[5]
 		return lshift(longStack[1], 28) + lshift(longStack[2], 21) + lshift(longStack[3], 14)
 			+ lshift(longStack[4], 7) + longStack[5]
 	end
 
 	byteArray.readUTF = function(self)
-		local byte = self:read8(self:read16())
+		local byte = self:read(self:read16())
 
 		if type(byte) == "number" then
 			return char(byte)
@@ -154,7 +167,7 @@ do
 	end
 
 	byteArray.readBigUTF = function(self)
-		local byte = self:read8(self:read24())
+		local byte = self:read(self:read24())
 
 		if type(byte) == "number" then
 			return char(byte)
