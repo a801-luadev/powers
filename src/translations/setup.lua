cc7d9d694e32475cee51f53954e1eca93399912f
Lautenschlager-id <tai.romani@hotmail.com>
2020-06-16 23:44:56 -0300

    fix merge

--- src/translations/setup.lua
+++ src/translations/setup.lua
@@ -1,19 +1,39 @@
 do
+table.merge = function(this,src)
+	for k, v in next, src do
+		if this[k] then
+			if type(v) == "table" then
+				this[k] = table.turnTable(this[k])
+				table.merge(this[k], v)
+			else
+				this[k] = this[k] or v
+			end
+		else
+			this[k] = v
+		end
+	end
+end
+
 	local merge
-	merge = function(src, aux)
+	merge = function(src, aux, ignoredIndexes)
 		for k, v in next, aux do
-			if type(v) == "table" and type(src[k]) == "table" then
-				merge(v, src[k])
-			elseif not src[k] then
-				src[k] = v
+			if not ignoredIndexes[k] then
+				if type(v) == "table" then
+					src[k] = merge((type(src[k]) == "table" and src[k] or { }), v, ignoredIndexes)
+				else
+					src[k] = src[k] or v
+				end
 			end
 		end
+		return src
 	end
 
 	getText = translations[tfm.get.room.community]
 	if getText then
 		if getText ~= translations.en then
-			merge(getText, translations.en)
+			merge(getText, translations.en, {
+				["levelName"] = true
+			})
 		end
 	else
 		getText = translations.en
